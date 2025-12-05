ea7a7d36af355132cd9a1886dfb8b0808b20feb8
Twenne Elffers <twenelf@gmail.com>
2025-12-05 03:50:25 +0100

    Updated the python to run on ns 3.46

--- cc_RTT_comparison.py
+++ cc_RTT_comparison.py
@@ -19,7 +19,20 @@
 #   - buffer = 1 × BDP
 #   - Simulation length = 40s, applications start from t = 1s
 
-from ns import ns
+try:
+    from ns import ns
+except ModuleNotFoundError:
+    raise SystemExit(
+        "Error: ns3 Python module not found;"
+        " Python bindings may not be enabled"
+        " or your PYTHONPATH might not be properly configured"
+    )
+from ctypes import c_bool, c_int
+
+verbose = c_bool(True)
+if verbose.value:
+    ns.LogComponentEnable("UdpEchoClientApplication", ns.LOG_LEVEL_INFO)
+    ns.LogComponentEnable("UdpEchoServerApplication", ns.LOG_LEVEL_INFO)
 
 # --------- Global parameters -------------
 PKT_SIZE = 1500         # bytes
@@ -62,16 +75,17 @@ def calc_bdp_pkts(data_rate_str: str, rtt_sec: float, pkt_size: int) -> float:
 
 
 def build_dumbbell_topology(data_rate_str: str, rtt_sec: float):
+def build_dumbbell_topology2(data_rate_str: str, rtt_sec: float):
     """
     Construct a dumbbell topology:
 
-        n0 ----\
-                \
-                 R0 ====== R1
-                /          \
-        n1 ----/            \
-                              n2
-                              n3
+             _                                 _
+            |    n1------+        +------n4     |
+            |            |        |             |
+    Senders |    n2------R1------R2-----n5      |    Receivers
+            |            |        |             |
+            |_   n3------+        +------n6    _|
+  
 
     Node indices:
         0: n0 (left sender 1)
@@ -90,18 +104,44 @@ def build_dumbbell_topology(data_rate_str: str, rtt_sec: float):
         nodes: NodeContainer with 6 nodes
         (ip_n2, ip_n3): IPv4 addresses of n2 and n3
     """
-    nodes = ns.NodeContainer()
-    nodes.Create(6)
+    networkTop = '''
+    Network Topology:
+             _                                 _
+            |    n1------+        +------n4     |
+            |            |        |             |
+    Senders |    n2------R1------R2-----n5      |    Receivers
+            |            |        |             |
+            |_   n3------+        +------n6    _|
+    '''
+    #print(networkTop)
+
+
+    # nodes = ns.NodeContainer()
+    # nodes.Create(6)
+
+    senders = ns.NodeContainer()
+    senders.Create(3)
+
+    routers = ns.NodeContainer()
+    routers.Create(2)
+
+    receivers = ns.NodeContainer()
+    receivers.Create(3)
 
     stack = ns.InternetStackHelper()
-    stack.Install(nodes)
+    stack.Install(senders)
+    stack.Install(receivers)
+    stack.Install(routers)
 
     # Non-bottleneck links: fast, 0ms delay
-    edge = ns.PointToPointHelper()
-    edge.SetDeviceAttribute("DataRate", ns.StringValue("1Gbps"))
-    edge.SetChannelAttribute("Delay", ns.StringValue("0ms"))
+    # print("setting up non-bottleneck links")
+    edge = ns.CsmaHelper()
+    edge.SetChannelAttribute("DataRate", ns.StringValue("1Mbps"))
+    edge.SetChannelAttribute("Delay", ns.StringValue("2ms"))
+
 
     # Bottleneck: controlled RTT
+    # print("setting up bottleneck links")
     bottleneck = ns.PointToPointHelper()
     bottleneck.SetDeviceAttribute("DataRate", ns.StringValue(data_rate_str))
 
@@ -109,43 +149,37 @@ def build_dumbbell_topology(data_rate_str: str, rtt_sec: float):
     bottleneck.SetChannelAttribute("Delay", ns.StringValue(f"{one_way_ms}ms"))
 
     address = ns.Ipv4AddressHelper()
+    # print("setting up c0")
 
-    # n0 -- R0
-    c0 = ns.NodeContainer(nodes.Get(0), nodes.Get(2))
+    # n1,n2,n3 -- R0
+    c0 = ns.NodeContainer()
+    c0.Add(routers.Get(0)) # router R1
+    c0.Add(senders) # n1,n2,n3
     dev0 = edge.Install(c0)
     address.SetBase(ns.Ipv4Address("10.1.1.0"), ns.Ipv4Mask("255.255.255.0"))
-    _ = address.Assign(dev0)
+    address.Assign(dev0)
 
-    # n1 -- R0
-    c1 = ns.NodeContainer(nodes.Get(1), nodes.Get(2))
+    # print("setting up c1")
+    # R1 -- n4, n5, n6
+    c1 = ns.NodeContainer()
+    c1.Add(routers.Get(1)) # router R2
+    c1.Add(receivers) # n4 n5 n6
     dev1 = edge.Install(c1)
-    address.SetBase(ns.Ipv4Address("10.1.2.0"), ns.Ipv4Mask("255.255.255.0"))
-    _ = address.Assign(dev1)
-
-    # R0 -- R1 (bottleneck)
-    cb = ns.NodeContainer(nodes.Get(2), nodes.Get(3))
-    devb = bottleneck.Install(cb)
     address.SetBase(ns.Ipv4Address("10.1.3.0"), ns.Ipv4Mask("255.255.255.0"))
-    _ = address.Assign(devb)
+    recAddresses = address.Assign(dev1)
 
-    # R1 -- n2
-    c2 = ns.NodeContainer(nodes.Get(3), nodes.Get(4))
-    dev2 = edge.Install(c2)
-    address.SetBase(ns.Ipv4Address("10.1.4.0"), ns.Ipv4Mask("255.255.255.0"))
-    iface2 = address.Assign(dev2)
-
-    # R1 -- n3
-    c3 = ns.NodeContainer(nodes.Get(3), nodes.Get(5))
-    dev3 = edge.Install(c3)
-    address.SetBase(ns.Ipv4Address("10.1.5.0"), ns.Ipv4Mask("255.255.255.0"))
-    iface3 = address.Assign(dev3)
+    # print("setting up R0-R1")
+    # R0 -- R1 (bottleneck)
+    devb = bottleneck.Install(routers.Get(0), routers.Get(1)) # R0-R1
+    address.SetBase(ns.Ipv4Address("10.1.2.0"), ns.Ipv4Mask("255.255.255.0"))
+    address.Assign(devb)
 
     ns.Ipv4GlobalRoutingHelper.PopulateRoutingTables()
 
-    ip_n2 = iface2.GetAddress(1)
-    ip_n3 = iface3.GetAddress(1)
+    ip_n2 = recAddresses.GetAddress(2)
+    ip_n3 = recAddresses.GetAddress(3)
 
-    return nodes, (ip_n2, ip_n3)
+    return (senders, routers, receivers), (ip_n2, ip_n3), edge
 
 
 def run_single_flow(data_rate_str, rtt_sec, cca_name, cca_type_str):
@@ -157,7 +191,6 @@ def run_single_flow(data_rate_str, rtt_sec, cca_name, cca_type_str):
         - variable: RTT
     """
     set_tcp_cca(cca_type_str)
-
     bdp_pkts = calc_bdp_pkts(data_rate_str, rtt_sec, PKT_SIZE)
     queue_pkts = int(round(bdp_pkts * BUF_FACTOR))
 
@@ -166,7 +199,7 @@ def run_single_flow(data_rate_str, rtt_sec, cca_name, cca_type_str):
         ns.StringValue(f"{queue_pkts}p")
     )
 
-    nodes, (ip_n2, _) = build_dumbbell_topology(data_rate_str, rtt_sec)
+    (senders, routers, receivers), (ip_n2, _), edge = build_dumbbell_topology2(data_rate_str, rtt_sec)
 
     port = 8080
 
@@ -174,7 +207,7 @@ def run_single_flow(data_rate_str, rtt_sec, cca_name, cca_type_str):
     sock_addr = ns.InetSocketAddress(ip_n2, port)
     sink_addr = sock_addr.ConvertTo()
     sink_helper = ns.PacketSinkHelper("ns3::TcpSocketFactory", sink_addr)
-    sink_apps = sink_helper.Install(nodes.Get(4))
+    sink_apps = sink_helper.Install(receivers.Get(1))
     sink_apps.Start(ns.Seconds(0.0))
     sink_apps.Stop(ns.Seconds(SIM_TIME))
 
@@ -183,12 +216,19 @@ def run_single_flow(data_rate_str, rtt_sec, cca_name, cca_type_str):
     bulk.SetAttribute("MaxBytes", ns.UintegerValue(0))
     bulk.SetAttribute("SendSize", ns.UintegerValue(PKT_SIZE))
 
-    client = bulk.Install(nodes.Get(0))
+    client = bulk.Install(senders.Get(0))
     client.Start(ns.Seconds(APP_START))
     client.Stop(ns.Seconds(SIM_TIME))
 
+
+    trace_file = ascii.CreateFileStream("cc_rtt_single_" + cca_name + f"_rtt{int(rtt_sec*1000)}ms.tr")
+    edge.EnableAsciiAll(trace_file)
+
+
+
     ns.Simulator.Stop(ns.Seconds(SIM_TIME))
     ns.Simulator.Run()
+
 
     total_rx = sink_apps.Get(0).GetTotalRx()
 
@@ -221,7 +261,7 @@ def run_competing_flows(data_rate_str, rtt_sec, start2=5.0):
         ns.StringValue(f"{queue_pkts}p")
     )
 
-    nodes, (ip_n2, ip_n3) = build_dumbbell_topology(data_rate_str, rtt_sec)
+    (senders, routers, receivers), (ip_n2, ip_n3) = build_dumbbell_topology2(data_rate_str, rtt_sec)
 
     set_node_tcp_cca(0, "ns3::TcpCubic")
     set_node_tcp_cca(1, "ns3::TcpVegas")
@@ -233,14 +273,15 @@ def run_competing_flows(data_rate_str, rtt_sec, start2=5.0):
     addr1 = ns.InetSocketAddress(ip_n2, port1)
     sink_addr1 = addr1.ConvertTo()
     sink_h1 = ns.PacketSinkHelper("ns3::TcpSocketFactory", sink_addr1)
-    sink_apps1 = sink_h1.Install(nodes.Get(4))
+    sink_apps1 = sink_h1.Install(receivers.Get(0))
     sink_apps1.Start(ns.Seconds(0.0))
     sink_apps1.Stop(ns.Seconds(SIM_TIME))
 
+    # Flow 1 sender
     bulk1 = ns.BulkSendHelper("ns3::TcpSocketFactory", sink_addr1)
     bulk1.SetAttribute("MaxBytes", ns.UintegerValue(0))
     bulk1.SetAttribute("SendSize", ns.UintegerValue(PKT_SIZE))
-    client1 = bulk1.Install(nodes.Get(0))
+    client1 = bulk1.Install(senders.Get(0))
     client1.Start(ns.Seconds(APP_START))
     client1.Stop(ns.Seconds(SIM_TIME))
 
@@ -248,14 +289,15 @@ def run_competing_flows(data_rate_str, rtt_sec, start2=5.0):
     addr2 = ns.InetSocketAddress(ip_n3, port2)
     sink_addr2 = addr2.ConvertTo()
     sink_h2 = ns.PacketSinkHelper("ns3::TcpSocketFactory", sink_addr2)
-    sink_apps2 = sink_h2.Install(nodes.Get(5))
+    sink_apps2 = sink_h2.Install(receivers.Get(2))
     sink_apps2.Start(ns.Seconds(0.0))
     sink_apps2.Stop(ns.Seconds(SIM_TIME))
 
+    # Flow 2 sender
     bulk2 = ns.BulkSendHelper("ns3::TcpSocketFactory", sink_addr2)
     bulk2.SetAttribute("MaxBytes", ns.UintegerValue(0))
     bulk2.SetAttribute("SendSize", ns.UintegerValue(PKT_SIZE))
-    client2 = bulk2.Install(nodes.Get(1))
+    client2 = bulk2.Install(senders.Get(1))
     client2.Start(ns.Seconds(start2))
     client2.Stop(ns.Seconds(SIM_TIME))
 
